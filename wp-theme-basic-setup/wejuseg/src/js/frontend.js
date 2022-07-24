8b94035778a6d6b48c09ea0f10f2cb091d482bc1
Alexandre Ménard <alex@lewebsimple.ca>
2022-07-24 18:03:25 -0400

    Add wp-theme-basic-setup

---
+++ wp-theme-basic-setup/wejuseg/src/js/frontend.js
@@ -0,0 +1,7 @@
+require.context('../images', true, /.*/);
+import $ from 'jquery';
+
+// Main 
+$(function () {
+    // App entry ...
+});
---
+++ wp-theme-basic-setup/wejuseg/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+const mode = process.argv.includes('production') ? 'production' : 'development';
+
+module.exports = {
+    devtool: mode === 'development' ? 'source-map' : false,
+    entry: {
+        frontend: './src/js/frontend.js',
+        style: './src/sass/style.scss',
+    },
+    output: {
+        filename: 'js/[name].js',
+        path: path.resolve(__dirname, 'assets'),
+        clean: true,
+    },
+    resolve: {
+    },
+    plugins: [
+        // Remove empty scripts for style entry
+        new RemoveEmptyScriptsPlugin(),
+        // Extract CSS from commonjs into seperate file
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css",
+        }),
+    ],
+    externals: {
+        jquery: 'jQuery',
+    },
+    module: {
+        rules: [
+            // Style
+            {
+                test: /\.s?css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            // Images
+            {
+                test: /\.(svg|png|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: './images/[name][ext]',
+                },
+            },
+        ],
+    },
+};
