3e4605ee3f4df788c69ef762d412906cbfd8a33c
Alexandre Ménard <alex@lewebsimple.ca>
2022-08-15 13:20:54 -0400

    fix: mixing postcss-loader

--- tailwind-3/webpack.config.js
+++ tailwind-3/webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
             // Style
             {
                 test: /\.s?css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
             },
             // Images
             {
